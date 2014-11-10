624bb891cb216661c4f6b5a59a4966990f95c6d1
alvas <alvations@gmail.com>
2014-11-10 22:13:53 +0100

    added truecasing to preprocess_europarl.py

--- europarl-mt/preprocess_europarl.py
+++ europarl-mt/preprocess_europarl.py
@@ -9,31 +9,35 @@ langs2links = literal_eval(io.open('europarl-links.txt', 'r').read())
 
 moses_script_path = "/home/alvas/mosesdecoder/scripts" 
 
-
 def get_prefix(src_lang, trg_lang):
     link = langs2links[src_lang, trg_lang]
     file_prefix = link.rpartition('/')[2].rpartition('.')[0].partition('.')[0]
     return 'Europarl.'+file_prefix
 
+
+############################################################################
+# Tokenization command
+############################################################################
+
 def tokenize_europarl_cmd_single(lang, prefix, infile_path='corpus.org', 
                                  outfile_path='corpus.tok',
                                  moses_script_path=moses_script_path,
-                                 experiment_path=os.getcwd(),
-                                 shutup=False):
+                                 experiment_path=os.getcwd(), shutup=False):
     
     # Initialize  cat with filename
     cat = "cat {d}/{i}/{p}.{l} |".format(d=experiment_path, i=infile_path, 
                                          p=prefix, l=lang)
     perl = "perl {m}/tokenizer/tokenizer.perl -l {l}".format(m=moses_script_path, l=lang)
-    outfile = " > {d}/{o}/train.tok.{l}".format(d=experiment_path, 
-                                                o=outfile_path, l=lang)
+    outfile = " > {d}/{o}/{p}.tok.{l}".format(d=experiment_path, 
+                                              o=outfile_path, p=prefix, 
+                                              l=lang)
     cmd = "sh -c '{} {} {}'".format(cat, perl, outfile)
     if shutup:
-        cmd = cmd + ">/dev/null 2>&1 &"
-    return [cmd]
+        cmd = cmd + ">/dev/null 2>&1"
+    return cmd +" &"
 
 def tokenize_europarl_cmd(src_lang, trg_lang, infile_path='corpus.org', 
-                          outfile_path='corpus.tok', shutup=False):
+                          outfile_path='corpus.tok',shutup=False):
     # Get Europarl prefix
     prefix = get_prefix(src_lang, trg_lang)
     src_cmd = tokenize_europarl_cmd_single(src_lang, prefix, infile_path, 
@@ -44,23 +48,98 @@ def tokenize_europarl_cmd(src_lang, trg_lang, infile_path='corpus.org',
                                            "${EXPERIMENT}", shutup)
     
     script_lines = ["\n"] +  ["# Tokenizing Europarl "+src_lang+'-'+trg_lang]
-    script_lines += src_cmd + trg_cmd + ['wait']
+    script_lines += [src_cmd]+ [trg_cmd] + ['wait']
     return script_lines 
     
+############################################################################
+# Cleaning command
+############################################################################
 
 def clean_europarl_cmd(src_lang, trg_lang, infile_path='corpus.tok', 
-                       minlen=1, maxlen=40,
-                      moses_script_path=moses_script_path, 
-                      shutup=False):
-    # Get Europarl prefix
-    prefix = get_prefix(src_lang, trg_lang)
-    
+                       minlen=1, maxlen=40, prefix =None, shutup=False, 
+                       truecase=True):
+    if not prefix:
+        if truecase:
+            prefix = get_prefix(src_lang, trg_lang)+'.tok'+'.truecase'
+        else:
+            prefix = get_prefix(src_lang, trg_lang)+'.tok'
+            
     change_directory = "cd {}".format("${EXPERIMENT}/"+infile_path)
     perl = "perl {m}/training/clean-corpus-n.perl {p} {sl} {tl} train-clean {min} {max}".format(
-           m="${MOSES_SCRIPT}", p=prefix,sl=src_lang, tl=trg_lang, 
+           m="${MOSES_SCRIPT}", p=prefix, sl=src_lang, tl=trg_lang, 
            min=minlen, max=maxlen)
     
+    if shutup:
+        perl = perl + ">/dev/null 2>&1"
     cmd = [change_directory] + [perl]
     script_lines = ["\n"] +  ["# Cleaning Europarl "+src_lang+'-'+trg_lang]
-    script_lines += cmd 
+    script_lines += cmd
     return script_lines
+
+############################################################################
+# Train Truecaser command
+############################################################################
+
+
+def train_truecase_europarl_cmd_single(lang, infile_path, prefix, shutup=False):
+    perl  = "perl ${MOSES_SCRIPT}/recaser/train-truecaser.perl"
+    model = "--model {ip}/truecase-model.{l} --corpus {ip}/{p}.{l}".format(ip=infile_path, l=lang, p=prefix)
+    train_cmd = "{} {}".format(perl, model)
+    if shutup:
+        train_cmd = train_cmd + ">/dev/null 2>&1"
+    
+    return train_cmd +" &"
+
+def train_truecase_europarl_cmd(src_lang, trg_lang, infile_path='corpus.tok',
+                                prefix=None, shutup=False):
+    if not prefix:
+        prefix = get_prefix(src_lang, trg_lang)+'.tok'        
+    src_cmd = train_truecase_europarl_cmd_single(src_lang, 
+                                                 "${EXPERIMENT}/"+infile_path, 
+                                                 prefix, shutup)
+    trg_cmd = train_truecase_europarl_cmd_single(trg_lang, 
+                                                 "${EXPERIMENT}/"+infile_path,
+                                                 prefix, shutup)
+    
+        
+    script_lines = ["\n"] +  ["# Training Truecaser Europarl "+src_lang+'-'+trg_lang]
+    script_lines += [src_cmd] + [trg_cmd] + ['wait']
+    return script_lines
+
+############################################################################
+# Truecasing command
+############################################################################
+
+
+def truecase_europarl_cmd_single(lang, infile_path, prefix, shutup=False):
+    perl = "${MOSES_SCRIPT}/recaser/truecase.perl"
+    model = "--model {}/truecase-model.{}".format(infile_path, lang)
+    inoutfile = "< {p}.tok.{l} > {p}.tok.truecase.{l}".format(l=lang, p=prefix)
+    cmd = "{} {} {}".format(perl, model, inoutfile)
+    if shutup:
+        cmd = cmd + ">/dev/null 2>&1"
+    
+    return cmd +" &"
+    
+
+def truecase_europarl_cmd(src_lang, trg_lang, infile_path='corpus.tok',
+                                prefix=None, shutup=False ):
+    if not prefix:
+        prefix = get_prefix(src_lang, trg_lang)+'.tok'   
+    src_cmd = truecase_europarl_cmd_single(src_lang, "${EXPERIMENT}/"+infile_path, 
+                                     prefix, shutup)
+    trg_cmd = truecase_europarl_cmd_single(trg_lang, "${EXPERIMENT}/"+infile_path,
+                                     prefix, shutup)
+    script_lines = ["\n"] +  ["# Truecasing Europarl "+src_lang+'-'+trg_lang]
+    script_lines += [src_cmd] + [trg_cmd] + ['wait']
+    return script_lines   
+     
+    
+
+'''
+def train_truecase_europarl_cmd(src_lang, trg_lang, prefix='train.tok'):
+   #${MOSES_SCRIPT}/recaser/train-truecaser.perl --model truecase-model.en --corpus train_dev.tok.en 
+   train_truecase_europarl_cmd_single(src_lang, prefix=)
+    
+${MOSES_SCRIPT}/recaser/truecase.perl --model truecase-model.en < ${file}.tok.en > ${file}.en
+'''
--- europarl-mt/script_europarl_mt.py
+++ europarl-mt/script_europarl_mt.py
@@ -4,26 +4,39 @@ import os
 
 from get_europarl import download_europarl_cmd
 from preprocess_europarl import tokenize_europarl_cmd, clean_europarl_cmd
+from preprocess_europarl import train_truecase_europarl_cmd, truecase_europarl_cmd
+
 from util import create_experiment
 
 moses_script_path = "/home/alvas/mosesdecoder/scripts"
+shutup=False
 
 # Creates experiment
 expname = "europarl_pbsmt_en_de"
 script = create_experiment(expname, moses_script_path)
 
-# Downloads Europarl
-dl_europarl = download_europarl_cmd('en', 'de', 'corpus.org') 
+# Download Europarl
+dl_europarl = download_europarl_cmd('en', 'de', 'corpus.org', 
+                                    shutup=shutup) 
 
 # Tokenize Europarl files
-tk_europarl = tokenize_europarl_cmd('en', 'de', 'corpus.org', 'corpus.tok')
+tk_europarl = tokenize_europarl_cmd('en', 'de', 'corpus.org', 'corpus.tok', 
+                                    shutup=shutup)
 
+
+# Train Truecaser Europarl files
+trtc_europarl = train_truecase_europarl_cmd('en', 'de', 'corpus.tok',
+                                          shutup=shutup)
+
+# Truecase Europarl files
+tc_europarl = truecase_europarl_cmd('en', 'de', 'corpus.tok', shutup=shutup)
 
 # Clean Europarl files
-cl_europarl = clean_europarl_cmd('en', 'de', 'corpus.tok', 1, 40)
+cl_europarl = clean_europarl_cmd('en', 'de', 'corpus.tok', 1, 40,  
+                                 shutup=shutup, truecase=True)
 
 
-script += dl_europarl + tk_europarl + cl_europarl
+script += dl_europarl + tk_europarl + trtc_europarl + tc_europarl +  cl_europarl
 
 for i in script:
     print i
